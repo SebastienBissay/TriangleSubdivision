3f40dd23e33bc486c4de4f8e2029c043aab166cd
SebastienBissay <seb.bissay@gmail.com>
2024-11-11 00:06:35 +0100

    Create README.md

--- src/main/java/parameters/Parameters.java
+++ src/main/java/parameters/Parameters.java
@@ -25,5 +25,41 @@ public final class Parameters {
         return Collections.singletonMap(Parameters.class.getSimpleName(), map);
     }
 
-    public record Color (float red, float green, float blue, float alpha) {}
+    public record Color (float red, float green, float blue, float alpha) {
+        public Color(float red, float green, float blue) {
+            this(red, green, blue, 255);
+        }
+
+        public Color(float grayscale, float alpha) {
+            this(grayscale, grayscale, grayscale, alpha);
+        }
+
+        public Color(float grayscale) {
+            this(grayscale, 255);
+        }
+
+        public Color(String hexCode) {
+            this(decode(hexCode));
+        }
+
+        public Color(Color color) {
+            this(color.red, color.green, color.blue, color.alpha);
+        }
+
+        public static Color decode(String hexCode) {
+            return switch (hexCode.length()) {
+                case 2 -> new Color(Integer.valueOf(hexCode, 16));
+                case 4 -> new Color(Integer.valueOf(hexCode.substring(0, 2), 16),
+                        Integer.valueOf(hexCode.substring(2, 4), 16));
+                case 6 -> new Color(Integer.valueOf(hexCode.substring(0, 2), 16),
+                        Integer.valueOf(hexCode.substring(2, 4), 16),
+                        Integer.valueOf(hexCode.substring(4, 6), 16));
+                case 8 -> new Color(Integer.valueOf(hexCode.substring(0, 2), 16),
+                        Integer.valueOf(hexCode.substring(2, 4), 16),
+                        Integer.valueOf(hexCode.substring(4, 6), 16),
+                        Integer.valueOf(hexCode.substring(6, 8), 16));
+                default -> throw new IllegalArgumentException();
+            };
+        }
+    }
 }
